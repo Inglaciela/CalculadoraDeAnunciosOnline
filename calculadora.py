11e768365be14b4f9be18abc31eebfb8b21cd02e
Bruna Inglaciela <82710857+Inglaciela@users.noreply.github.com>
2021-10-25 13:23:23 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+#a cada 100 pessoas que visualizam o anúncio 12 clicam nele.
+#a cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais.
+#cada compartilhamento  nas redes sociais gera 40 novas visualizações.
+#30 pessoas visualizam o anúncio original(não compartilhado) a cada R$ 1,00 investido.
+#o mesmo anúncio é compartilhado no máximo 4 vezes em sequência
+#-(1.pessoa->compartilha->2.pessoa->compartilha->3.pessoa->4.pessoa->compartilha).
+#crie um SCRIPT em sua linguagem de programação preferida que receba o valor investido
+#-em reais e retorne uma projeção aproximada de quantidade máxima de pessoas que visualizarão o mesmo anúncio
+#-(considerando o anúncio original + os compartilhamentos)
+
+nome = input('Digite o nome do Anúncio:')
+print('o Anuncio {}'.format(nome))
